8328d09fe0a73d85f4b1628865c86f95a9aba90c
Romane <romaneborde@gmail.com>
2019-10-17 21:05:17 +0200

    Init project

---
+++ DTRQXamarinApp/DTRQXamarinApp/DTRQXamarinApp/Views/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DTRQXamarinApp.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
