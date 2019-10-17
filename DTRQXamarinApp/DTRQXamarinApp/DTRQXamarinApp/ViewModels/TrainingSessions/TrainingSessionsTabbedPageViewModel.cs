5a750a725b27505f41eeac2a849e1c2472087425
Romane <romaneborde@gmail.com>
2019-10-17 22:42:12 +0200

    Creation of folders to separate differents parts (VM and V)

--- DTRQXamarinApp/DTRQXamarinApp/DTRQXamarinApp/ViewModels/TrainingSessions/TrainingSessionsTabbedPageViewModel.cs
+++ DTRQXamarinApp/DTRQXamarinApp/DTRQXamarinApp/ViewModels/TrainingSessions/TrainingSessionsTabbedPageViewModel.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace DTRQXamarinApp.ViewModels
+namespace DTRQXamarinApp.ViewModels.TrainingSessions
 {
     public class TrainingSessionsTabbedPageViewModel : ViewModelBase
     {
