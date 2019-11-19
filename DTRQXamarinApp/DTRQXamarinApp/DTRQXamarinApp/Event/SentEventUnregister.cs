8ba82b82ab62e51701ce527a014564b893cbbddb
Romane <romaneborde@gmail.com>
2019-11-19 08:56:13 +0100

    Move Class event to event folder

--- DTRQXamarinApp/DTRQXamarinApp/DTRQXamarinApp/Event/SentEventUnregister.cs
+++ DTRQXamarinApp/DTRQXamarinApp/DTRQXamarinApp/Event/SentEventUnregister.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace DTRQXamarinApp
+namespace DTRQXamarinApp.Event
 {
     public class SentEventUnregister : PubSubEvent<int>
     {
