18ec93ac439da107e3775c19b62c9ac7de2e850c
Romane <romaneborde@gmail.com>
2019-11-21 16:32:10 +0100

    Hotfix merge

---
+++ DTRQXamarinApp/DTRQXamarinApp/DTRQXamarinApp.Android/DatabaseConnection_Android.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using DTRQXamarin.Droid;
+using SQLite;
+
+[assembly: Xamarin.Forms.Dependency(typeof(DatabaseConnection_Android))]
+namespace DTRQXamarin.Droid
+{
+    public class DatabaseConnection_Android : IDatabaseConnection
+    {
+        public SQLiteConnection DbConnection(string dbPath)
+        {
+            var path = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal),"Android");
+            return new SQLiteConnection(path);
+        }
+    }
+}
